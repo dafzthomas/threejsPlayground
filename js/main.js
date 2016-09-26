283383f90daa9bd30aa8ac118825ff295ef56187
Dafydd Thomas <dafz@dafzthomas.com>
2016-09-26 18:06:48 +0100

    Added mouse movement and a plane... very basic

--- js/main.js
+++ js/main.js
@@ -3,6 +3,7 @@
     function onWindowResize () {
         camera.aspect = window.innerWidth / window.innerHeight;
         camera.updateProjectionMatrix();
+        controls.updateScreenSize();
 
         renderer.setSize( window.innerWidth, window.innerHeight );
     };
@@ -11,42 +12,67 @@
         requestAnimationFrame(render);
 
         marioCube.rotation.y += 0.01;
+        marioCube.rotation.x += 0.01;
 
-        controls.update();
+        delta = clock.getDelta();
+
+        controls.update(delta);
         renderer.render(scene, camera);
         stats.update();
     };
 
-    function cube (w, h, d, texture) {
+    function Cube (w, h, d, opts) {
         let geometry = new THREE.BoxGeometry(w, d, h);
         let material = new THREE.MeshBasicMaterial({
-            map: textureLoader.load(texture)
+            map: textureLoader.load(opts.texture)
         });
 
-        return cube = new THREE.Mesh(geometry, material);
+        return new THREE.Mesh(geometry, material);
     }
 
-    let scene, camera, renderer, controls, stats, textureLoader, light;
+    let scene, camera, renderer, clock, delta, controls, stats, textureLoader, light;
 
-    textureLoader = new THREE.TextureLoader();
+    clock = new THREE.Clock();
     scene = new THREE.Scene();
     camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 10000);
-    controls = new THREE.OrbitControls(camera);
+    // controls = new THREE.OrbitControls(camera);
+    controls = new THREE.FirstPersonControls(camera);
     renderer = new THREE.WebGLRenderer({
         antialias: true,
         alpha: true
     });
+    textureLoader = new THREE.TextureLoader();
 
-    camera.position.z = 5;
+    // Camera Options
+    camera.position.x = -20;
+    camera.position.y = 5;
+
+    // Controls Options
+    controls.movementSpeed = 20;
+    controls.lookSpeed = 0.3;
+    controls.noFly = true;
+    controls.constrainVertical = true;
     controls.minDistance = 5;
     controls.maxDistance = 50;
 
     renderer.setSize(window.innerWidth, window.innerHeight);
     document.body.appendChild(renderer.domElement);
 
-    let marioCube = cube(2, 2, 2, 'images/mario-block.png')
+    /* Floor  */
+    var floor = new THREE.Mesh(
+        new THREE.PlaneGeometry( 20, 20, 20, 20 ),
+        new THREE.MeshBasicMaterial( { color: 0x00ff30 } )
+    );
+    floor.material.side = THREE.DoubleSide;
+    floor.rotation.x = (Math.PI / 2);
+    // floor.rotation.y = 100;
+    // floor.translate(0, 0, 10);
+    scene.add(floor );
+
+    let marioCube = Cube(2, 2, 2, {texture: 'images/mario-block.png'});
 
     marioCube.rotation.x = 0.4;
+    marioCube.position.y = 2;
 
     scene.add(marioCube);
 
